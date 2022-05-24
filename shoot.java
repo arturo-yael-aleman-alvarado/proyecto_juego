bfec02644e30915449b8b9b20c0dacdcc8f81e8e
arturo-yael-aleman-alvarado <franmg555@gmail.com>
2022-05-23 19:39:11 -0500

    Initial commit

---
+++ shoot.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class shoot here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class shoot extends Actor
+{
+    /**
+     * Act - do whatever the shoot wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
